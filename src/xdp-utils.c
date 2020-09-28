b2973defbbe4a1172142695951b4ae2d4113179a
Gleb Popov <6yearold@gmail.com>
2020-09-28 20:18:47 +0400

    Fix build of src/xdp-utils.c on FreeBSD.

--- src/xdp-utils.c
+++ src/xdp-utils.c
@@ -29,7 +29,14 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <stdio.h>
+#ifdef HAVE_MNTENT_H
+#endif
+#ifdef HAVE_SYS_VFS_H
 #include <sys/vfs.h>
+#endif
+#ifdef HAVE_SYS_MOUNT_H
+#include <sys/mount.h>
+#endif
 
 #include <gio/gdesktopappinfo.h>
 
@@ -815,7 +822,7 @@ xdp_filter_options (GVariant *options,
 
           continue;
         }
-         
+
       if (supported_options[i].validate)
         {
           g_autoptr(GError) local_error = NULL;
